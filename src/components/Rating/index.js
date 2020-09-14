70fe5790f4ed5a4c024cc88acbc9adb2101aad6a
Beicker Zambrano <beicker998@gmail.com>
2020-09-14 16:32:29 -0500

    Add a rating count with useState

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Box } from "@material-ui/core"
 import { createStyles, withStyles } from "@material-ui/core"
 
@@ -10,29 +10,36 @@ const styles = () =>{
 			height:"auto",
 			maxWidth:"100%",
 			cursor:"pointer",
+		},
+		'@media screen and (min-width:1024px)':{
+			ratingCount:{
+				fontSize:"18px",
+			}
 		}
 	})
 }
 
 const Rating = ({classes})=>{
 
+	const [ratingCount, setRatingCount] = useState(3)
 	return(
 		<Box mt=".5rem" mb="1rem" width="60%" display="flex" justifyContent="space-between">
-			<Box height="1.75rem" width="17%">
+			<Box height="1.5rem" width="17%">
 				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height="1.75rem" width="17%">
+			<Box height="1.5rem" width="17%">
 				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height="1.75rem" width="17%">
+			<Box height="1.5rem" width="17%">
 				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height="1.75rem" width="17%">
+			<Box height="1.5rem" width="17%">
 				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height="1.75rem" width="17%">
+			<Box height="1.5rem" width="17%">
 				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
 			</Box>
+			<Box className={classes.ratingCount} component="label">{`(${ratingCount})`}</Box>	
 		</Box>
 	)
 }
