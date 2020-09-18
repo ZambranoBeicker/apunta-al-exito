fcb1b69529fd2e5cec5d2aa34b6fbc288379bd04
Beicker Zambrano <beicker998@gmail.com>
2020-09-18 18:52:00 -0500

    Switch color of a star

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Box } from "@material-ui/core"
 import { createStyles, withStyles } from "@material-ui/core"
 
@@ -11,6 +11,9 @@ const styles = () =>{
 			maxWidth:"100%",
 			cursor:"pointer",
 		},
+		displayDisable:{
+			display:"none",
+		},
 		'@media screen and (min-width:1024px)':{
 			ratingCount:{
 				fontSize:"18px",
@@ -22,22 +25,49 @@ const styles = () =>{
 const Rating = ({classes, wrapperMargin = ".5rem 0 1rem 0", wrapperWidth = "60%", starHeight = "1.5rem", isThereCount = true})=>{
 
 	const [ratingCount, setRatingCount] = useState(3)
+	const [starState, setStarState] = useState(false)
+
+	const switchStars = state =>{
+		if(state){
+			return classes.displayDisable 
+		}else{
+			return ""
+		} 
+	} 
+
+
+	const oppositeSwitchStars = state =>{
+		if(state){
+			return ""
+		}else{
+			return classes.displayDisable 
+		} 
+	}	
+	 
+
+
+	//const svgImg = document.querySelector(classes.star).getSVGDocument().getElementById("starSvg")
 	return(
 		<Box m={wrapperMargin} width={wrapperWidth} display="flex" justifyContent="space-between">
-			<Box height={starHeight} width="17%">
-				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
+			<Box onClick={()=>{setStarState(state => !state)}} display="flex" height={starHeight} width="17%">
+				<img className={classes.star + " " + switchStars(starState)} src="./svg/starActive.svg" alt="Estrella de Puntajes"/>
+				<img className={classes.star + " " + oppositeSwitchStars(starState)} src="./svg/starDisable.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height={starHeight} width="17%">
-				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
+			<Box display="flex" height={starHeight} width="17%">
+				<img className={classes.star} src="./svg/starActive.svg" alt="Estrella de Puntajes"/>
+				<img className={classes.star + " " + classes.displayDisable} src="./svg/starDisable.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height={starHeight} width="17%">
-				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
+			<Box display="flex" height={starHeight} width="17%">
+				<img className={classes.star} src="./svg/starActive.svg" alt="Estrella de Puntajes"/>
+				<img className={classes.star + " " + classes.displayDisable} src="./svg/starDisable.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height={starHeight} width="17%">
-				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
+			<Box display="flex" height={starHeight} width="17%">
+				<img className={classes.star} src="./svg/starActive.svg" alt="Estrella de Puntajes"/>
+				<img className={classes.star + " " + classes.displayDisable} src="./svg/starDisable.svg" alt="Estrella de Puntajes"/>
 			</Box>
-			<Box height={starHeight} width="17%">
-				<img className={classes.star} src="./svg/star.svg" alt="Estrella de Puntajes"/>
+			<Box display="flex" height={starHeight} width="17%">
+				<img className={classes.star} src="./svg/starActive.svg" alt="Estrella de Puntajes"/>
+				<img className={classes.star + " " + classes.displayDisable} src="./svg/starDisable.svg" alt="Estrella de Puntajes"/>
 			</Box>
 			{ (isThereCount) && <Box className={classes.ratingCount} component="label">{`(${ratingCount})`}</Box> }	
 		</Box>
